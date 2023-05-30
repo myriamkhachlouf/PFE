3ab7ce361a88ee0999824967b505d3a3cfedb224
myriamkhachlouf <47786254+myriamkhachlouf@users.noreply.github.com>
2023-05-30 17:17:11 +0100

    final before redirection

--- src/app/deals/deals.component.ts
+++ src/app/deals/deals.component.ts
@@ -10,6 +10,22 @@ export class DealsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var divElement = document.getElementById('viz1685389586139');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) {
+      vizElement.style.width = '1200px';
+      vizElement.style.height = '727px';
+    } else if (divElement.offsetWidth > 500) {
+      vizElement.style.width = '1200px';
+      vizElement.style.height = '727px';
+    } else {
+      vizElement.style.width = '100%';
+      vizElement.style.height = '827px';
+    }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  
   }
 
 }
